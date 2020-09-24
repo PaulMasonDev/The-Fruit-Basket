8e843d98a230e35eef6a753d9054e0e6e20d679c
Paul <webdevpaulmason@gmail.com>
2020-09-24 16:14:06 -0700

    Setup cart item render

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 import './Cart.css'
 
-import Fruit from '../Fruit/Fruit';
+import CartItem from '../CartItem/CartItem';
 
 const Cart = ( { fruitData, updateCart }) => {
   return (
-    <div>
-      <h1>I'm the Cart Component</h1>
+    <div className="Cart">
+      <h1>SHOPPING CART</h1>
       { fruitData &&
         fruitData.map(fruit => {
-          return <Fruit key={fruit.id} fruit={fruit} updateCart={updateCart}/>
+          return <CartItem key={fruit.id} fruit={fruit} updateCart={updateCart}/>
         })
       }
     </div>
--- client/src/components/CartItem/CartItem.js
+++ client/src/components/CartItem/CartItem.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './CartItem.css'
+import Counter from '../Counter/Counter';
 
-const CartItem = ( { }) => {
+const CartItem = ( { fruit, updateCart }) => {
+  
   return (
-    <div>
-      <h1>I'm the CartItem Component</h1>
+    <div className="CartItem">
+      <img className="CartItem__img" src={fruit.url} />
+      <div className="CartItem__counter-container">
+        <Counter updateCart={updateCart} />
+        <p>{fruit.title}</p>
+        <p>{(fruit.qty * fruit.price).toFixed(2)}</p>
+      </div>
     </div>
-    
   );
 }
 
